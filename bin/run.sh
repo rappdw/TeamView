d6424d832e8459cdfb5032192afcc1188265ad3a
Dan Rapp <drapp@proofpoint.com>
2019-02-25 10:36:35 -0700

    detect kubectl (or microk8s.kubectl) and exit if not present

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
-kubectl delete -f team-view.yaml
-kubectl create -f team-view.yaml
+KC_CMD="kubectl"
+if ! [ -x "$(command -v kubectl)" ]; then
+    if ! [ -x "$(command -v microk8s.kubectl)" ]; then
+        echo 'unable to find kubectl command. please ensure Kubenetes is properly installed'
+        exit 1
+    fi
+    KC_CMD="microk8s.kubectl"
+fi
+
+
+$KC_CMD delete -f team-view.yaml
+$KC_CMD create -f team-view.yaml
 sleep 30
-export NODE_PORT=$(kubectl get services/team-view -o go-template='{{(index .spec.ports 0).nodePort}}')
+export NODE_PORT=$($KC_CMD get services/team-view -o go-template='{{(index .spec.ports 0).nodePort}}')
 open http://localhost:$NODE_PORT/lab
